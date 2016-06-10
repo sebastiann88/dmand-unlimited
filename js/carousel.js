e8dcd06231ee3cc973d38c1b2bfb84cc654d197d
sebastiann88 <sebastian_chmura@hotmail.com>
2016-06-10 08:29:42 -0400

    style(all): add placeholder image to carousel

--- js/carousel.js
+++ js/carousel.js
@@ -69,6 +69,19 @@ function marqueeMultiPanel(){
         marqueeVars.autoPlay = true;
         marqueeVars.timePassed = Math.floor(marqueeVars.timetoChange / 2);
     });
+    
+    $('.marquee_nav div').on('click', function(){
+        var navClicked = $(this).index();
+        marqueeVars.currentPanel = navClicked + 1;
+        
+        $('.marquee_stage_large').append('<div class="marquee_container_2" style="opacity:0;"></div>');
+        $('.marquee_container_2').html(marqueeVars.panelContent[navClicked]).animate({opacity:1},marqueeVars.duration,function(){
+            $('.marquee_container_1').remove();
+            $(this).addClass('marquee_container_1').removeClass('marquee_container_2');
+        });
+    });
+    
+    $('.marquee_nav div:first').trigger('click');
 }
 
 //debugger
